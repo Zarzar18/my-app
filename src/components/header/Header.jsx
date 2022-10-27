bdf79c56f4adc7ffbac7db2398af925506606edd
Zarzar18 <nizararifin0918@gmail.com>
2022-10-28 00:32:56 +0700

    personal-task

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="container">
+      <div>Nizar Todo List</div>
+    </div>
+  );
+}
+export default Header;
